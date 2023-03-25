8417c20957df90c90399aadada3cf236698a1870
so1454 <122778253+so1454@users.noreply.github.com>
2023-03-25 10:43:35 +0900

    0325

--- Jquery/footer,header_auto_search.js
+++ Jquery/footer,header_auto_search.js
@@ -94,3 +94,17 @@ List = ["에브리씽 에브리웨어 올 앳 원스",
 "내가 사는 피부",
 "더 퀸",
 "가재가 노래하는 곳"];
+
+
+
+$(function () {	//화면 로딩후 시작
+    $("#searchInput").autocomplete({  //오토 컴플릿트 시작
+        source: List,	// source는 data.js파일 내부의 List 배열
+        focus : function(event, ui) { // 방향키로 자동완성단어 선택 가능하게 만들어줌	
+            return false;
+        },
+        minLength: 1,// 최소 글자수
+        delay: 100,	//autocomplete 딜레이 시간(ms)
+        //disabled: true, //자동완성 기능 끄기
+    });
+});
